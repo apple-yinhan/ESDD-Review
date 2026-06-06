c22580cfa7251da506166b454f918af01b169451
apple-yinhan <hanyin@kaist.ac.kr>
2026-06-06 17:33:05 +0900

    main

--- eval.py
+++ eval.py
@@ -2,27 +2,11 @@ from data_utils import genSpoof_list, ADD_Dataset, eval_to_score_file
 import json
 
 print('====== Evaluation ======')
-dir_path = '.track_2_label.json'
-eval_output = '.track_2_b02.txt'
-# target_generators = ['audiolcm', 'tangoflux', 'audioldm2'] 
-target_generators = ['foleycrafter', 'diff_foley']
+dir_path = './data/track_2_label.json'
+eval_output = './data/track_2_b02.txt'
 
 print('test data path: ', dir_path)
 with open(dir_path, 'r') as f:
         data = json.load(f)
 print('no. of test trials',len(data))
 eer = eval_to_score_file(eval_output, dir_path)
-
-for target_generator in target_generators:
-    print(f'Filter data with target audio generator: {target_generator}')
-    filtered_data = []
-    for item in data:
-        if item['generative_model'] == target_generator or item['generative_model'] == 'none':
-            filtered_data.append(item)
-
-    output_path = dir_path.replace('.json', f'_{target_generator}.json')
-    print('no. of test trials',len(filtered_data))
-    with open(output_path, 'w') as f:
-        json.dump(filtered_data, f, indent=4)
-    
-    eer = eval_to_score_file(eval_output, output_path)
